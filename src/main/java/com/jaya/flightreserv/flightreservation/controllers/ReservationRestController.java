68ac9bb13ef7e1d86f67ce3fa5def706d761f8c8
JAYAKUMARRAMAR <jayakumar92@gmail.com>
2026-06-02 21:16:57 +0530

    created flightcheckin frontend project

--- src/main/java/com/jaya/flightreserv/flightreservation/controllers/ReservationRestController.java
+++ src/main/java/com/jaya/flightreserv/flightreservation/controllers/ReservationRestController.java
@@ -1,6 +1,7 @@
 package com.jaya.flightreserv.flightreservation.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import com.jaya.flightreserv.flightreservation.entities.Reservation;
 import com.jaya.flightreserv.flightreservation.repos.ReservationRepository;
 
 @RestController
+@CrossOrigin
 public class ReservationRestController {
    
     @Autowired
