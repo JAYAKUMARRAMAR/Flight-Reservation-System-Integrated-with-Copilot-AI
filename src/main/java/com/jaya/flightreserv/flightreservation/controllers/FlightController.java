2bfd3ffb8fec9224a101f12de176812bdd3af90b
JAYAKUMARRAMAR <jayakumar92@gmail.com>
2026-06-02 12:54:48 +0530

    update find flights

--- src/main/java/com/jaya/flightreserv/flightreservation/controllers/FlightController.java
+++ src/main/java/com/jaya/flightreserv/flightreservation/controllers/FlightController.java
@@ -1,14 +1,35 @@
-//flight controller class with a displayFindFlights method that returns the thymeleaf template
 package com.jaya.flightreserv.flightreservation.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.jaya.flightreserv.flightreservation.entities.Flight;
+import com.jaya.flightreserv.flightreservation.repos.FlightRepository;
 
-@RestController
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
 public class FlightController {
-    @GetMapping("/findFlights")
-    public String displayFindFlights(Model model) {
-        return "findFlights";
+    
+    @Autowired
+    private FlightRepository flightRepository;
+    
+    @PostMapping("/findFlights")
+    public String displayFindFlights(
+            @RequestParam("from") String from,
+            @RequestParam("to") String to,
+            @RequestParam("departureDate") @DateTimeFormat(pattern = "MM-dd-yyyy") LocalDate departureDate,
+            Model model) {
+        
+        List<Flight> flights = flightRepository.findFlights(from, to, departureDate);
+        model.addAttribute("flights", flights);
+        
+        return "displayFlights";
     }
+
+    
 }
--- src/main/java/com/jaya/flightreserv/flightreservation/repos/FlightRepository.java
+++ src/main/java/com/jaya/flightreserv/flightreservation/repos/FlightRepository.java
@@ -1,23 +1,16 @@
 package com.jaya.flightreserv.flightreservation.repos;
 
-import com.jaya.flightreserv.flightreservation.entities.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import com.jaya.flightreserv.flightreservation.entities.Flight;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Repository
-public interface FlightRepository extends JpaRepository<Flight, Long> {
-    List<Flight> findByDepartureCity(String departureCity);
-    
-    List<Flight> findByArrivalCity(String arrivalCity);
-    
-    List<Flight> findByDepartureCityAndArrivalCity(String departureCity, String arrivalCity);
-    
-    List<Flight> findByDateOfDeparture(LocalDate dateOfDeparture);
-    
-    List<Flight> findByOperatingAirline(String operatingAirline);
-    
-    List<Flight> findByFlightNumber(String flightNumber);
+public interface FlightRepository extends JpaRepository<Flight, Long> 
+{
+	@Query("SELECT f FROM Flight f WHERE f.departureCity = :from AND f.arrivalCity = :to AND f.dateOfDeparture = :departureDate")
+	List<Flight> findFlights(String from, String to, LocalDate departureDate);
 }
