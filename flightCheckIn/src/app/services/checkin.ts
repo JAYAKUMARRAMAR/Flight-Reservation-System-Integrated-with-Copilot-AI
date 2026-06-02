060cca8532f72884417b9765aeeb4662ea7692e5
JAYAKUMARRAMAR <jayakumar92@gmail.com>
2026-06-02 23:45:44 +0530

    test home page

--- flightCheckIn/src/app/services/checkin.ts
+++ flightCheckIn/src/app/services/checkin.ts
@@ -1,6 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Checkin {}
+export class Checkin {
+  private apiUrl = 'http://localhost:8080/flightreservation/reservations';
+
+  constructor(private http: HttpClient) {}
+
+  getReservation(id: string | number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  updateReservation(reservation: any): Observable<any> {
+    const url = `${this.apiUrl}`;
+    return this.http.post<any>(url, reservation);
+  }
+}
