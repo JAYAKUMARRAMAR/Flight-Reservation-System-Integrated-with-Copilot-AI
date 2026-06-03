5341a25d23bbef62bcea5186f2e405bbb4c00114
JAYAKUMARRAMAR <jayakumar92@gmail.com>
2026-06-03 09:30:22 +0530

    checkin completed

--- flightCheckIn/src/app/components/checkin/checkin.ts
+++ flightCheckIn/src/app/components/checkin/checkin.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { Checkin as CheckinService } from '../../services/checkin';
 
 @Component({
   selector: 'app-checkin',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './checkin.html',
-  styleUrl: './checkin.css',
+  styleUrls: ['./checkin.css'],
 })
-export class Checkin {}
+export class Checkin {
+  reservationData: any;
+  data: any;
+  noOfbags: any;
+  constructor(private service: CheckinService, private router: Router) {}
+  ngOnInit() {
+    this.data = this.service.reservationData;
+  }
+
+  checkIn() {
+    let request = {
+      id: this.data.id,
+      checkedIn: true,
+      numberOfBags: this.noOfbags,
+    };
+    this.service.updateReservation(request).subscribe((data: any) => {
+      console.log(data);
+      this.router.navigate(['/confirm']);
+    });
+  }
+}
